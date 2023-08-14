19e88810c7ab4944c0595cf20244f5bbaee4d3b3
Philippe ROSTAN <81040730+PhilippeR26@users.noreply.github.com>
2023-08-14 11:56:05 +0200

    build: cairo210

--- scripts/compileCairo.sh
+++ scripts/compileCairo.sh
@@ -1,2 +1,2 @@
 cd cairo
-cargo run --bin starknet-compile -- --allowed-libfuncs-list-file ../scripts/lib_funcs.json ../src/hello/hello.cairo ../out/hello/hello.sierra.json
+cargo run --bin starknet-compile -- --single-file --allowed-libfuncs-list-file ../scripts/lib_funcs.json ../src/hello/hello.cairo ../out/hello/hello.sierra.json
