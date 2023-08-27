715afd0a704f85b75c53d80b0064f2e0df42c1df
Philippe ROSTAN <81040730+PhilippeR26@users.noreply.github.com>
2023-08-27 22:26:18 +0200

    Build: adds

---
+++ scripts/compileOZerc721-070.sh
@@ -0,0 +1,3 @@
+# go to Cairo v2.2.0 directory
+cargo run --bin starknet-compile -- /home/edmond/Documents/starknet/OpenZeppelin/cairo-contracts/src/ ../out/cairo220/OpenZeppelin/erc721OZ070.sierra.json --contract-path openzeppelin::token::erc721::erc721::ERC721
+cargo run --bin starknet-sierra-compile --  ../out/cairo220/OpenZeppelin/erc721OZ070.sierra.json ../out/cairo220/OpenZeppelin/erc721OZ070.casm.json
