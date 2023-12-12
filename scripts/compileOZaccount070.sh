20c9d228e09c46d9aff996e678a7ae6de9dbcc3e
PhilippeR26 <philippe.rostan@yahoo.fr>
2023-12-12 15:12:58 +0100

    cairo240-1

--- scripts/compileOZaccount070.sh
+++ scripts/compileOZaccount070.sh
@@ -1,3 +1,3 @@
 # go to Cairo v2.2.0 directory
-cargo run --bin starknet-compile -- /home/edmond/Documents/starknet/OpenZeppelin/cairo-contracts/src/ ../out/cairo220/OpenZeppelin/accountOZ070.sierra.json --contract-path openzeppelin::account::account::Account
-cargo run --bin starknet-sierra-compile --  ../out/cairo220/OpenZeppelin/accountOZ070.sierra.json ../out/cairo220/OpenZeppelin/accountOZ070.casm.json
+cargo run  --release --bin starknet-compile -- /home/edmond/Documents/starknet/OpenZeppelin/cairo-contracts/src/ ../out/cairo220/OpenZeppelin/accountOZ070.sierra.json --contract-path openzeppelin::account::account::Account
+cargo run --release --bin starknet-sierra-compile --  ../out/cairo220/OpenZeppelin/accountOZ070.sierra.json ../out/cairo220/OpenZeppelin/accountOZ070.casm.json
--- scripts/compileSierra.sh
+++ scripts/compileSierra.sh
@@ -1,4 +1,4 @@
-cargo run --bin starknet-sierra-compile -- ../out/hello/hello.sierra.json  ../out/hello/hello.casm.json --allowed-libfuncs-list-file ../scripts/lib_funcs.json
+cargo run --release --bin starknet-sierra-compile -- ../out/hello/hello.sierra.json  ../out/hello/hello.casm.json --allowed-libfuncs-list-file ../scripts/lib_funcs.json
 
 # launch devnet with
 # starknet-devnet --seed 0 --verbose --compiler-args '--allowed-libfuncs-list-file /D/Cairo1-dev/scripts/lib_funcs.json --add-pythonic-hints' 2> /dev/null &
